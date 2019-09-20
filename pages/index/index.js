63a5555725e711eccd51f1bcfa942a6cca7aedec
yuboyang <smileyby@126.com>
2019-09-20 17:16:43 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,7 +6,15 @@ const router = require('../../routes/index.js');
 
 Page({
   data: {
-    motto: 'Amazing',
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+    indicatorDots: true,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
   },
   onLoad: function () {
     console.log('App onLoad');
